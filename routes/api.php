9c1f9bc4c030f1e279b666c96f90d4a19ab68d00
yuayudia <73992618+yuayudia@users.noreply.github.com>
2021-07-23 19:56:11 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -35,6 +35,20 @@ Route::post('/ayudia/', 'API\BarangController@store');
 Route::delete('/ayudia/{id_barang}', 'API\BarangController@destroy');
 Route::patch('/ayudia/{id_barang}', 'API\BarangController@update');
 
+//WAJIB LOGIN SUPLIER//
+Route::get('/suplier', 'API\SuplierController@index')->middleware('auth:api');
+Route::get('/suplier/{id_suplier}', 'API\SuplierController@show')->middleware('auth:api');
+Route::post('/suplier/', 'API\SuplierController@store')->middleware('auth:api');
+Route::delete('/suplier/{id_suplier}', 'API\SuplierController@destroy')->middleware('auth:api');
+Route::patch('/suplier/{id_suplier}', 'API\SuplierController@update')->middleware('auth:api');
+
+//BEBAS AKSES SUPLIER//
+Route::get('/supbas', 'API\SuplierController@index');
+Route::get('/supbas/{id_suplier}', 'API\SuplierController@show');
+Route::post('/supbas/', 'API\SuplierController@store');
+Route::delete('/supbas/{id_suplier}', 'API\SuplierController@destroy');
+Route::patch('/supbas/{id_suplier}', 'API\SuplierController@update');
+
 Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
