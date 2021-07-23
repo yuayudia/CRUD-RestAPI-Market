f50fbb4bc9541f2d5ca969e95b69c8684e3049a5
yuayudia <73992618+yuayudia@users.noreply.github.com>
2021-07-23 17:21:56 +0800

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(BarangSeeder::class);
-        $this->call(SuplierSeeder::class);
+        $this->call(BarangSeeder::class);
     }
 }
