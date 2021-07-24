3cc0f2147e8a1b431e31604964da83ba9ccafb75
yuayudia <73992618+yuayudia@users.noreply.github.com>
2021-07-24 12:42:38 +0800

    Update suplier.php

--- app/suplier.php
+++ app/suplier.php
@@ -9,4 +9,9 @@ class suplier extends Model
     protected $primaryKey = 'id_suplier';
     public $timestamps = true;
     protected $fillable = ['nama_suplier','alamat','telepon','pos','email'];
+    
+    public function barang()
+    {
+    return $this->hasMany(Barang::class, 'id_suplier');
+    }
 }
